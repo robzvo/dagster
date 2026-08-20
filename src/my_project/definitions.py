edbd805a1bcba38f78042e533989cb8fff297a8e
robzvo <robzvo@users.noreply.github.com>
2026-08-20 10:53:49 -0400

    moved defs to proper location

--- src/my_project/definitions.py
+++ src/my_project/definitions.py
@@ -1,8 +1,18 @@
 from pathlib import Path
 
-from dagster import definitions, load_from_defs_folder
+from dagster import Definitions
+
+from defs.assets import dg_assets
+from defs.jobs import dg_jobs
+from resources import dg_resources
 
 
-@definitions
-def defs():
-    return load_from_defs_folder(path_within_project=Path(__file__).parent)
+# @definitions
+# def defs():
+#     return load_from_defs_folder(path_within_project=Path(__file__).parent)
+
+defs = Definitions(
+    assets= dg_assets,
+    jobs=dg_jobs,
+    resources=dg_resources
+)
---
+++ src/my_project/defs/jobs.py
@@ -0,0 +1,14 @@
+import time
+from dagster import Definitions, job, op
+
+@op
+def my_op():
+    time.sleep(30)
+    return True
+
+@job
+def my_job():
+    my_op()
+
+
+dg_jobs = [my_job]
